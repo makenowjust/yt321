2f00589e71cd895efdf7154667fe6b8b909fab6e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2014-11-16 19:13:46 +0900

    rewrite yt321

--- bin/yt321.js
+++ bin/yt321.js
@@ -2,135 +2,48 @@
 
 var
 debug = require('debug')('yt321'),
+init = require('../lib/init.js'),
 package = require('../package.json');
 
 var
 yargs = require('yargs')
-  .usage('Usage:\n  $0 [-dzl] [-@ FILE]... URL...\n  $0 -s WORD')
-  .version(package.version, 'version').alias('version', 'V')
+  .usage('Usage:\n  $0 [-1dfilqz] [-@ FILE]... URL...\n  $0 [-1] -s WORD\n  $0 [-hV]')
+  .boolean('download').alias('download', 'd')
+  .describe('download', 'Download mp3')
+  .boolean('force').alias('force', 'f')
+  .describe('force', 'Force downloading specified Youtube URL, not use cache')
   .help('help').alias('help', 'h')
-  .boolean('s').alias('s', 'search')
-  .describe('s', 'Search WORD in youtube')
-  .boolean('l').alias('l', 'loop')
-  .describe('l', 'Play in endless loop')
-  .boolean('z').alias('z', 'random')
-  .describe('z', 'Random play specified urls')
-  .boolean('d').alias('d', 'download')
-  .describe('d', 'Download mp3')
-  .boolean('f').alias('f', 'force')
-  .describe('f', 'Force downloading specified Youtube URL, not use cache')
-  .string('@').alias('@', 'list')
-  .describe('@', 'Specify play list file'),
-argv = yargs.argv;
+  .boolean('info').alias('info', 'i')
+  .describe('info', 'Show information of YoutubeURL\'s video')
+  .string('list').alias('list', '@')
+  .describe('list', 'Specify play list file')
+  .boolean('loop').alias('loop', 'l')
+  .describe('loop', 'Play in endless loop')
+  .boolean('one-line').alias('one-line', '1')
+  .describe('one-line', 'Print information as one line for peco and percol')
+  .boolean('quiet').alias('quiet', 'q')
+  .describe('quiet', 'Not print downloading & convert information and more')
+  .boolean('random').alias('random', 'z')
+  .describe('random', 'Random play specified urls')
+  .boolean('search').alias('search', 's')
+  .describe('search', 'Search WORD in youtube')
+  .version(package.version, 'version').alias('version', 'V'),
+options = yargs.argv;
 
-if (argv.search) {
-  search(argv._);
-} else if (argv._.length >= 1 || (argv.list && argv.list.length >= 1)) {
-  if (argv.download) {
-    download(argv._, argv.list || [], argv.force);
+// init
+init(options);
+
+if (options.search) {
+  require('../lib/search.js')(options);
+} else if (options._.length >= 1 || (options.list && options.list.length >= 1)) {
+  if (options.info) {
+    require('../lib/info.js')(options);
+  } else if (options.download) {
+    require('../lib/download.js')(options);
   } else {
-    play(argv._, argv.loop, argv.random, argv.list || [], argv.force);
+    require('../lib/play.js')(options);
   }
 } else {
-  console.error('no specified -s or URL');
-  yargs.showHelp();
-}
-
-function search (word) {
-  var
-  search = require('youtube-search');
-  if (!Array.isArray(word)) {
-    word = [word];
-  }
-  word = word.join(' ');
-  debug('[search] search ' + word);
-
-  search(word, {
-    maxResults: 10,
-    startIndex: 1,
-  }, function (err, results) {
-    if (err) throw err;
-    debug('[search] finish search');
-
-    results.forEach(function (result) {
-      console.log('title: ' + result.title);
-      if (result.description) console.log('description: ' + result.description);
-      console.log('url: ' + '\'' + result.url + '\'');
-      console.log();
-    });
-  });
-}
-
-function download(urls, list, force) {
-  var
-  async = require('async'),
-  ytdl = require('ytdl-core'),
-  fs = require('fs'),
-  yt321 = require('..'),
-  path = require('path');
-  if (!Array.isArray(list)) list = [list];
-  debug('[download] start', urls);
-  debug('[download] list', list);
-  debug('[download] force', force);
-
-  async.concat(list, yt321.readPlayList, function (err, urlss) {
-    if (err) throw err;
-    debug('[play] list urls', urlss);
-
-    async.each(urls.concat(urlss), function (url, next) {
-      yt321.mp3path(force, url, function (err, p) {
-        if (err) next(err);
-        ytdl.getInfo(url, function (err, info) {
-          if (err) next(err);
-
-          var
-          p2 = path.join(process.cwd(), info.title + '.mp3');
-          fs.createReadStream(p)
-            .pipe(fs.createWriteStream(p2))
-            .on('finish', function () {
-              debug('[download] finish ' + url);
-              console.log(url + ' => ' + p2);
-              next(null);
-            });
-        });
-      });
-    }, function (err) {
-      if (err) throw err;
-      debug('[download] finish all');
-    });
-  });
-}
-
-function play(urls, loop, random, list, force) {
-  var
-  async = require('async'),
-  yt321 = require('..'),
-  mpg321 = require('mpg321');
-  if (!Array.isArray(list)) list = [list];
-  debug('[play] start', urls);
-  debug('[play] list', list);
-  debug('[play] force', force);
-
-  async.concat(list, yt321.readPlayList, function (err, urlss) {
-    if (err) throw err;
-    debug('[play] list urls', urlss);
-
-    async.map(urls.concat(urlss), yt321.mp3path.bind(yt321, force), function (err, ps) {
-      if (err) throw err;
-      var
-      m = mpg321(), child;
-      if (loop) {
-        debug('[play] loop');
-        m.loop(0);
-      }
-      if (random) {
-        debug('[play] random');
-        m.random();
-      }
-      debug('[play] mpg321 ' + ps.join(' '));
-      m.file.apply(m, ps);
-      child = m.exec();
-      process.on('SIGINT', child.kill.bind(child));
-    });
-  })
+  console.error('no specified effective options');
+  console.log(yargs.help());
 }
